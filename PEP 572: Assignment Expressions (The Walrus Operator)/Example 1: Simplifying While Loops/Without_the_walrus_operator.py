5a1896950801e1612311683e384f450d5d64fae3
TalaatMagdy <15043001+talaatmagdyx@users.noreply.github.com>
2024-06-15 00:59:20 +0000

    PEP 572: Assignment Expressions (The Walrus Operator)#Example 1: Simplifying While Loops

---
+++ PEP 572: Assignment Expressions (The Walrus Operator)/Example 1: Simplifying While Loops/Without_the_walrus_operator.py
@@ -0,0 +1,4 @@
+line = input("Enter a line: ")
+while line != "quit":
+    print(f"You entered: {line}")
+    line = input("Enter a line: ")
---
+++ PEP 572: Assignment Expressions (The Walrus Operator)/With_the_walrus_operator.py
@@ -0,0 +1,2 @@
+while (line := input("Enter a line: ")) != "quit":
+    print(f"You entered: {line}")
